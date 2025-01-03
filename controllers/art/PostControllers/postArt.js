b802b6b1b8f2927a5ac1755f2bd288d7aec987db
ShashankAquimo <schaurasia@aquimo.com>
2025-01-03 23:14:16 +0530

    Pushed basic structure for art

---
+++ controllers/art/PostControllers/postArt.js
@@ -0,0 +1,8 @@
+const { db } = require('../../../database/databaseConnection');
+
+const uploadArt = async (req, res) => {
+
+};
+module.exports = {
+
+}
---
+++ routes/art/artRoutes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+//GET ROUTE
+
+//POST ROUTE
+
+//PUT ROUTE
+
+//DELETE ROUTE
+
+module.exports = router;
